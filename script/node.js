e021b832a624fe217e1a8fd7619b2623c32dff5e
Tyrell Wellick <keshavniure99@gmail.com>
2025-08-27 09:19:17 -0800

    remove not working part

--- script/node.js
+++ script/node.js
@@ -10,24 +10,6 @@ taskInput.addEventListener('input', () => {
     }
 });
 // for task add on card
-const task = document.getElementById('taskInput');
-addTask.addEventListener('click', () => {
-   let taskValue = task.value;
-    const tasks = ` <div class="card display">
-                    <div class="one display"><input type="checkbox">
-                        <div class="text">${task.value} </div></div>
-                    <div class="two">
-                         <div class="remove">
-                            <img src="image/trash.svg" width="30" alt="">
-                         </div>
-                    </div>
-                </div>`;
-
-    console.log(taskValue);
-    
-
-        });
-
         document.querySelector('.button').addEventListener('click', function() {
             console.log("hello");
         });
