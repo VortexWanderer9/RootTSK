424c840e5454ad64c8a8ecdd668f4139caba572f
Tyrell Wellick <keshavniure99@gmail.com>
2025-08-28 12:47:34 -0800

    implement delete feature

--- script/node.js
+++ script/node.js
@@ -13,21 +13,30 @@ taskInput.addEventListener('input', () => {
 // for task add on card
         document.querySelector('.button').addEventListener('click', () => {
             let task = taskInput.value;
-            const taskContent = ` 
-            <div class="one display"><input type="checkbox" id="complete">
-            <div class="text">${task}</div></div>
-            <div class="two">
-            <div class="remove">
-            <img src="image/trash.svg" width="30" alt="">
-            </div>
-            </div>`
+            if(task.trim() === ''){
+                alert('please write down task')
+                return;
+            }
+                const taskContent = ` 
+                <div class="one display"><input type="checkbox" id="complete">
+                <div class="text">${task}</div></div>
+                <div class="two">
+                <div class="remove">
+                <img src="image/trash.svg" width="30" alt="">
+                </div>
+                </div>`
                 const div = document.createElement('div')
                 div.classList.add('card')
                 div.classList.add('display')
                 div.innerHTML = taskContent;
                 document.querySelector('.cards').appendChild(div)
-                taskInput.value = ''
+                taskInput.value = '';
+                 const remove = div.querySelector('.remove')
+                 remove.addEventListener('click', (e) =>{
+                    e.preventDefault()
+                    div.remove()
+                 });
+
+                 const 
+
         });
-
-
-
