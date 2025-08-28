ce7d29ad8931f45b1135cd7edba466cdfc6c840c
Tyrell Wellick <keshavniure99@gmail.com>
2025-08-28 07:44:46 -0800

    implement task management functionality and modularize task handling

--- script/node.js
+++ script/node.js
@@ -1,6 +1,7 @@
 let taskInput = document.getElementById('taskInput');
 let button = document.querySelector('.button');
 let addTask = document.querySelector('.active');
+import {taskCode } from './taskCont.js';
 
 taskInput.addEventListener('input', () => {
     if (taskInput.value.trim().length > 0) {
@@ -10,6 +11,25 @@ taskInput.addEventListener('input', () => {
     }
 });
 // for task add on card
-        document.querySelector('.button').addEventListener('click', function() {
-            console.log("hello");
+        document.querySelector('.button').addEventListener('click', () => {
+            let task = taskInput.value;
+            const taskContent = ` <div class="card display">
+                    <div class="one display"><input type="checkbox" id="taskInput">
+                        <div class="text">${task}</div></div>
+                    <div class="two">
+                         <div class="remove">
+                            <img src="image/trash.svg" width="30" alt="">
+                         </div>
+                    </div>
+                </div>`
+            taskInput.value = "";
+                taskCode.push(taskContent);
+                renderTasks()
         });
+
+        function renderTasks() {
+            document.querySelector('.cards').innerHTML = '';
+            taskCode.forEach(task => {
+                document.querySelector('.cards').insertAdjacentHTML('beforeend', task);
+            });
+        }
---
+++ script/taskCont.js
@@ -0,0 +1,4 @@
+export const taskCode = [];
+
+    console.log(taskCode[0]);
+
