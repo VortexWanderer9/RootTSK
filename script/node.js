b238a2c5262f857babad07b6eae1963f445bb16e
Tyrell Wellick <keshavniure99@gmail.com>
2025-08-28 14:52:04 -0800

    update task management UI and functionality; adjust active task count, enhance task completion handling, and improve styles for better visibility

--- script/node.js
+++ script/node.js
@@ -1,7 +1,7 @@
 let taskInput = document.getElementById('taskInput');
 let button = document.querySelector('.button');
 let addTask = document.querySelector('.active');
-import {taskCode } from './taskCont.js';
+import {taskCode, completedTasks } from './taskCont.js';
 
 taskInput.addEventListener('input', () => {
     if (taskInput.value.trim().length > 0) {
@@ -30,13 +30,58 @@ taskInput.addEventListener('input', () => {
                 div.classList.add('display')
                 div.innerHTML = taskContent;
                 document.querySelector('.cards').appendChild(div)
+                checkEmpty()
                 taskInput.value = '';
                  const remove = div.querySelector('.remove')
                  remove.addEventListener('click', (e) =>{
                     e.preventDefault()
                     div.remove()
+                    checkEmpty()
                  });
-
-                 const 
-
+                 const comp = div.querySelector('#complete')
+                 comp.addEventListener('change', () => {
+                    if(comp.checked){
+                        div.classList.add('completed');
+                        if (!completedTasks.includes(comp)) {
+                            completedTasks.push(comp);
+                        }
+                    } else {
+                        div.classList.remove('completed');
+                        const idx = completedTasks.indexOf(comp);
+                        if (idx !== -1) {
+                            completedTasks.splice(idx, 1);
+                        }
+                        console.log(idx);
+                        
+                    }
+                    checkComplete(completedTasks.length)
+                    checkEmpty()
+                 })
         });
+    let active;
+     function checkEmpty(){
+        const isThere = document.querySelectorAll('.card')
+        const defaults = document.querySelector('.default')
+       active = isThere.length
+        if(isThere.length > 0){
+            defaults.style.display = "none";
+        }else{
+            defaults.style.display = "";
+        }
+     }
+     
+     function checkComplete(complete) {
+        const checkCompleteEle = document.querySelector('.checkComplete')
+        if(!checkCompleteEle){
+            return;
+        }
+        document.querySelector('.completeNum').innerHTML = complete
+        checkCompleteEle.innerHTML = `Clear ${complete} completed task`
+                document.querySelector('#activeTask').innerHTML = active - complete
+        if (completedTasks.length == 0) {
+            checkCompleteEle.style.display = 'none'
+        }else{
+            checkCompleteEle.style.display = ''
+        }
+     }
+    
